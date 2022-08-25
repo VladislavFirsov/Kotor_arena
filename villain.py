5a102b56b36092b2634ab3fca0f0f69d49df3034
Vladislav Firsov <Valacardin@gmail.com>
2022-08-25 19:18:25 +0300

    refactored a lot of code repetition, created base class for inheritance

--- villain.py
+++ villain.py
@@ -1,12 +1,2 @@
-from random import choice
-from player import Player
-
-class Villain(Player):
-    __random_names = []
-
-    def __init__(self):
-        super().__init__(choice(self.__random_names), 10, 10, 50)
-
-
-a = Villain()
-print(a.__dict__)
+class Villain():
+    pass
