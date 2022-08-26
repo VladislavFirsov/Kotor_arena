0b8b2829e62b261b1732c1afa8c6575de0e55842
Vladislav Firsov <Valacardin@gmail.com>
2022-08-26 22:37:27 +0300

    created an inventory for the player and added some methods

--- player.py
+++ player.py
@@ -1,70 +1,83 @@
-#still have to add show weapon and armor in inventory
+#still have to add show weapon and armor in inventory and buy/sell functions
+#try to implement an automatic lvl_up if possible later
+#redo upgrade_char system
+from base_class.base_player import BasePlayer
 
-class Player:
-    __exp_to_lvl_up = {1: 0, 2: 100, 3: 300, 4: 600, 5: 1200, 6: 2400, 7: 5000, 8: 10000, 9: 15000, 10: 25000}
 
+class Inventory:
+    inventory = [[], []]
+
+    def add_to(self, value):
+        if isinstance(value, Weapon):
+            self.inventory[0].append(value)
+        elif isinstance(value, Armor):
+            self.inventory[1].append(value)
+
+    def take_from(self, value):
+        if isinstance(value, Weapon):
+            for i in self.inventory[0]:
+                if value == i.name:
+                    return self.inventory[0].pop(i)
+        elif isinstance(value, Armor):
+            for i in self.inventory[0]:
+                if value == i.name:
+                    return self.inventory[1].pop(i)
+
+    def show_weapon(self):
+        for i in self.inventory[0]:
+            print(i)
+
+    def show_armor(self):
+        for i in self.inventory[1]:
+            print(i)
+
+
+class Player(BasePlayer):
     upgrade_points = 0
-
+    experience = BasePlayer.Characteriscics()
+    balance = BasePlayer.Characteriscics()
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super().__new__(cls)
         return cls.instance
 
-    def __init__(self, name, strength=8, dexterity=8, vitality=50):
-        self.__name = name
-        self.__lvl = 1
-        self.__body = None
-        self.__left_hand = None
-        self.__right_hand = None
+    def __init__(self, name):
+        super().__init__(name, 8, 8, 40, 8, 8, 9)
+        self.inventory = Inventory()
         self.experience = 0
-        self.strength = strength
-        self.dexterity = dexterity
-        self.vitality = vitality
+        self.__lvl = 1
         self.balance = 0
-        self.max_health = vitality
-        self.flurry = 1
-        self.power_attack = 1
-
-
-
-    def buy_medkit(self, shop, medkit):
-        temp = shop.sell_medkit(medkit)
-        if self.balance >= temp.price:
-            self.inventory.add_to_inventory(temp)
-            self.balance -= temp.price
 
     def lvl_up(self):
-        self.upgrade_points += 3
-        self.__lvl += 1
+        if len(str(self.experience)) == 4 and self.experience % 1000 == 0 and self.__lvl <= 10:
+            self.upgrade_points += 3
+            self.__lvl += 1
 
     def __check_points(self):
         return self.upgrade_points > 0
 
-    def upgrade_dexterity(self):
+    def upgrade_chars(self, char):
         if self.__check_points():
-            self.dexterity += 1
-            self.upgrade_points -= 1
-
-    def upgrade_strength(self):
-        if self.__check_points():
-            self.strength += 1
-            self.upgrade_points -= 1
-
-    def upgrade_vitality(self):
-        if self.__check_points():
-            self.vitality += 15
-            self.upgrade_points -= 1
-
-    def show_lvl(self):
-        print(f'{self.__lvl}  {self.experience}/{self.__exp_to_lvl_up[self.__lvl + 1]}')
+            if char == 'strength':
+                self.strength += 1
+            elif char == 'dexterity':
+                self.dexterity += 1
+            elif char == 'vitality':
+                self.vitality += 10
+            elif char == 'intelligence':
+                self.intelligence += 1
+            else:
+                self.wisdom += 1
+                self.force_points += 3
+        self.upgrade_points -= 1
 
     def show_chars(self):
-        print(f'strength: {self.strength}\nvitality: {self.vitality}\ndexterity: {self.dexterity}')
+        print(f'strength: {self.strength}\nvitality: {self.vitality}\ndexterity: {self.dexterity}\n'
+              f'intelligence: {self.intelligence}\nwisdom: {self.wisdom}')
 
 
 
-vlad = Player('vlad')
 
 
 
