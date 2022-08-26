f861657011ba434346e60466b13d37c6df75a6de
Vladislav Firsov <Valacardin@gmail.com>
2022-08-26 22:38:16 +0300

    deleted unnecessary functions

--- shop.py
+++ shop.py
@@ -1,30 +1,25 @@
 from random import choice
 
+
 class Armor:
-    def __init__(self, model, defence_bonus):
-        self.model = model
+    def __init__(self, name, defence_bonus):
+        self.name = name
         self.defence_bonus = defence_bonus
         self.dexterity_bonus = None
 
-    def __str__(self):
-        return self.model
-
 
 class Weapon:
-    def __init__(self, model, damage):
-        self.model = model
+    def __init__(self, name, damage):
+        self.name = name
         self.damage = damage
 
     def hit(self):
         return choice(self.damage)
 
-    def __str__(self):
-        return self.model
 
 
 
-class Arsenal:
-    pass
+
 
 
 
