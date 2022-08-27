8d5b7e3305af969faedb20e1cf7a2f29f7d1ef0d
Vladislav Firsov <Valacardin@gmail.com>
2022-08-28 01:17:05 +0300

    almost completed player

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
-#still have to add show weapon and armor in inventory and buy/sell functions
 #try to implement an automatic lvl_up if possible later
-#redo upgrade_char system
+#create abilities
 from base_class.base_player import BasePlayer
+from arsenal.arsenal import Weapon, Armor
 
 
 class Inventory:
@@ -13,15 +13,11 @@ class Inventory:
         elif isinstance(value, Armor):
             self.inventory[1].append(value)
 
-    def take_from(self, value):
-        if isinstance(value, Weapon):
-            for i in self.inventory[0]:
-                if value == i.name:
-                    return self.inventory[0].pop(i)
-        elif isinstance(value, Armor):
-            for i in self.inventory[0]:
-                if value == i.name:
-                    return self.inventory[1].pop(i)
+    def take_from(self, name):
+        for bag in self.inventory:
+            for item in bag:
+                if item.name == name:
+                    return bag.pop(bag.index(item))
 
     def show_weapon(self):
         for i in self.inventory[0]:
@@ -33,48 +29,74 @@ class Inventory:
 
 
 class Player(BasePlayer):
-    upgrade_points = 0
+    __upgrade_points = 0
     experience = BasePlayer.Characteriscics()
     balance = BasePlayer.Characteriscics()
+    lvl = BasePlayer.Characteriscics()
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super().__new__(cls)
         return cls.instance
 
-    def __init__(self, name):
+    def __init__(self, name, experience, balance, lvl):
         super().__init__(name, 8, 8, 40, 8, 8, 9)
         self.inventory = Inventory()
-        self.experience = 0
-        self.__lvl = 1
-        self.balance = 0
+        self.experience = experience
+        self.lvl = lvl
+        self.balance = balance
 
     def lvl_up(self):
-        if len(str(self.experience)) == 4 and self.experience % 1000 == 0 and self.__lvl <= 10:
-            self.upgrade_points += 3
-            self.__lvl += 1
+        if len(str(self.experience)) == 4 and self.experience % 1000 == 0 and self.lvl <= 10:
+            self.__upgrade_points += 3
+            self.lvl += 1
 
-    def __check_points(self):
-        return self.upgrade_points > 0
+    def __check_points(self, number):
+        return self.__upgrade_points - number >= 0
 
-    def upgrade_chars(self, char):
-        if self.__check_points():
+    def upgrade_chars(self, char, number):
+        if self.__check_points(number):
             if char == 'strength':
-                self.strength += 1
+                self.strength += number
             elif char == 'dexterity':
-                self.dexterity += 1
+                self.dexterity += number
             elif char == 'vitality':
-                self.vitality += 10
+                self.vitality += number
             elif char == 'intelligence':
-                self.intelligence += 1
+                self.intelligence += number
             else:
-                self.wisdom += 1
-                self.force_points += 3
-        self.upgrade_points -= 1
+                self.wisdom += number
+                self.force_points += number * 3
+        self.__upgrade_points -= number
 
     def show_chars(self):
         print(f'strength: {self.strength}\nvitality: {self.vitality}\ndexterity: {self.dexterity}\n'
               f'intelligence: {self.intelligence}\nwisdom: {self.wisdom}')
+
+    @property
+    def upgrade_points(self):
+        return self.__upgrade_points
+
+    def equip_weapon(self, name):
+        blade = self.inventory.take_from(name)
+        if blade.one_handed and self.right_hand is not None:
+            self.inventory.add_to(self.right_hand)
+            self.right_hand = blade
+        else:
+            self.inventory.add_to(self.right_hand)
+            self.right_hand = blade
+            self.left_hand = blade
+
+    def equip_armor(self, name):
+        armor = self.inventory.take_from(name)
+        if self.body is not None:
+            self.inventory.add_to(self.body)
+        self.body = armor
+
+    def sell_item(self, shop, name):
+        item = self.inventory.take_from(name)
+        self.balance += item.price // 2
+        #add shop logic
 
 
 
