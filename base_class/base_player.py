1ad8092367a004d81f2d598674ca95e1a8e526fa
Vladislav Firsov <Valacardin@gmail.com>
2022-08-28 01:16:35 +0300

    added some changes

--- base_class/base_player.py
+++ base_class/base_player.py
@@ -1,13 +1,15 @@
+from arsenal.arsenal import Weapon, Armor
+
 class BasePlayer:
     class Characteriscics:
         def __set_name__(self, owner, name):
-            self.name = f'__{name}'
+            self.name = f'_{name}'
 
         def __get__(self, instance, owner):
             return getattr(instance, self.name)
 
         def __set__(self, instance, value):
-            if type(value) is int and value > 0:
+            if type(value) is int and value >= 0:
                 instance.__dict__[self.name] = value
 
     strength = Characteriscics()
@@ -18,10 +20,10 @@ class BasePlayer:
     force_points = Characteriscics()
 
     def __init__(self, name, strength, dexterity, vitality, intelligence, wisdom, force_points):
-        self.__name = name
-        self.__body = None
-        self.__right_hand = None
-        self.__left_hand = None
+        self._name = name
+        self._body = None
+        self._right_hand = None
+        self._left_hand = None
         self.strength = strength
         self.dexterity = dexterity
         self.vitality = vitality
@@ -31,30 +33,30 @@ class BasePlayer:
 
     @property
     def body(self):
-        return self.__body
+        return self._body
 
     @body.setter
     def body(self, value):
         if isinstance(value, Armor):
-            self.__body = value
+            self._body = value
 
     @property
     def right_hand(self):
-        return self.__right_hand
+        return self._right_hand
 
     @right_hand.setter
     def right_hand(self, value):
         if isinstance(value, Weapon):
-            self.__right_hand = value
+            self._right_hand = value
 
     @property
     def left_hand(self):
-        return self.__left_hand
+        return self._left_hand
 
     @left_hand.setter
     def left_hand(self, value):
-        if isinstance(value, Weapon) and self.__right_hand:
-            self.__left_hand = value
+        if isinstance(value, Weapon) and self._right_hand:
+            self._left_hand = value
 
     def __str(self):
-        return self.__name
+        return self._name
