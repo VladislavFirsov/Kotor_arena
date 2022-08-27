789d797164ff308d5dda390a8b5495486f370e5a
Vladislav Firsov <Valacardin@gmail.com>
2022-08-28 01:17:33 +0300

    started

--- shop.py
+++ shop.py
@@ -1,20 +1,14 @@
-from random import choice
+class Shop:
+    armor = []
+    weaponry = []
+
+    def show_armor(self):
+        print(*self.armor)
 
 
-class Armor:
-    def __init__(self, name, defence_bonus):
-        self.name = name
-        self.defence_bonus = defence_bonus
-        self.dexterity_bonus = None
 
 
-class Weapon:
-    def __init__(self, name, damage):
-        self.name = name
-        self.damage = damage
 
-    def hit(self):
-        return choice(self.damage)
 
 
 
