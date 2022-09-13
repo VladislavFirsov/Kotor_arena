eb76f6e9a5fba430b29005054576c7eee93a59b6
Vladislav Firsov <Valacardin@gmail.com>
2022-09-13 22:52:38 +0300

    fixed some bags with database

--- game_process/game.py
+++ game_process/game.py
@@ -1,7 +1,7 @@
 import sqlite3
 from players.player import Player
 from game_process.shop import Shop
-#inventory not saving
+#inventory not saving, change the database on a new, clear one
 #add armor and weapon through shop method sell in the final file
 
 class GameCommands:
@@ -27,7 +27,7 @@ class GameCommands:
             c.execute('INSERT INTO player VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                       [self.player.name, self.player.strength, self.player.dexterity,
                        self.player.vitality, self.player.intelligence, self.player.wisdom,
-                       self.player.balance, self.player.lvl, self.player.experience,
+                       self.player.experience, self.player.balance, self.player.lvl,
                        self.player.body.name, self.player.right_hand.name])
 
     def load_game(self):
@@ -35,11 +35,11 @@ class GameCommands:
             c = db.cursor()
 
             features = c.execute('SELECT * from player ORDER BY id DESC LIMIT 1').fetchall()
-            self.player = Player(features[1], features[2], features[3],
-                                 features[4], features[5], features[6],
-                                 features[7], features[8], features[9])
-            self.player.body = Shop.sell_armor(Shop, features[10])
-            weapon = Shop.sell_weapon(Shop, features[-1])
+            self.player = Player(features[0][1], features[0][2], features[0][3],
+                                 features[0][4], features[0][5], features[0][6],
+                                 features[0][7], features[0][8], features[0][9])
+            self.player.body = Shop.sell_armor(Shop, features[0][10])
+            weapon = Shop.sell_weapon(Shop, features[0][-1])
             if not weapon.one_handed:
                 self.player.right_hand = self.player.left_hand = weapon
             else:
